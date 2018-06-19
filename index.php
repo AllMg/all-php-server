4a264ed01e86b8966537ea40a215e460775ce6ad
ravosoa <ravosoaravosoa@gmail.com>
2018-06-19 15:42:52 +0300

    new build

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 $servername = "mysql://mysql:3306/";
 $database = "sampledb";
-$username = "user5WQ";
-$password = "BTbVj6OBK3vyWiIJ";
+$username = "free";
+$password = "free";
 
 // Create connection
-/*$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password);
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
-}*/
+}
 echo "Connected successfully without Mysqli";
